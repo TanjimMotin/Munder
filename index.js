a075cf5a0676af22b0a452def8c71379ce79009d
TanjimMotin <tanjimotin@gmail.com>
2018-10-01 15:17:29 -0400

    more addition

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
+// ///////////////////////////// slideshow animation
+    let current = 0,
+    slides = document.getElementsByClassName("slide");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    setInterval(function() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = 0;
+    }
+    current = (current != slides.length - 1) ? current + 1 : 0;
+    slides[current].style.opacity = 1;
+    }, 3000);
 
 
 
